8faf8e4cc3b3741f5306e22d93377e7437bd7b3c
Vladimir <vladimirdegty@gmail.com>
2021-05-16 09:48:37 +0300

    main

---
+++ cmake-build-debug/CMakeFiles/KR1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KR1.dir/main.cpp.obj"
+  "KR1.exe"
+  "KR1.exe.manifest"
+  "KR1.lib"
+  "KR1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KR1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
